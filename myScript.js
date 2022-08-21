2a220c38a3ff277a1136d6b2dfa4e45eaa375bc6
Rohanc28 <rochaturvedi01@gmail.com>
2022-08-21 18:42:40 +0530

    Create myScript.js

---
+++ myScript.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".auto-type", {
+      strings: ["^4000 I am a Software and Web Developer. ^1000 <br><br>^1000 Senior Year B.tech student in CSE at Jaypee University of Information technology, Solan.<br><br>A problem solver, organised and passionate coder with attention to detail.<br><br>Fan of sports, TV series and love to read novels in free time.<br><br>I currently live in New Delhi, India."],
+      typeSpeed: 60,
+      loop: false
+    })
+    var typed = new Typed(".achi", {
+      strings: [" ^12000 IEEE Spin-o-web May 2022: Frontend Contest: 2nd Place</b> ^1000 <br>^1000<br><b>Murious Hackathon Mar 2022: 2nd Place:</b><br> Created working prototype, which digitalized traditional vending machines. Worked on creating the website + helped the team setup payments using RazorPay API gateway and output using Arduino ^1000 <br>^1000<br>ESummit'21 AlterEgo Oct 2021: UI: 2nd Place: Rethinking and Redesigning Tech companies Logos.  Second Place for Redesigning Yahoo! logo^1000 <br>^1000<br>HackerRank:^400 <br>6 Star: Problem Solving^400<br>5 Star: C++ and Python^400<br>5 Star: 30 days of Code</ul>"],
+      typeSpeed: 60,
+      loop: false
+    })
