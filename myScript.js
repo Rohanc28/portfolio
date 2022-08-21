1b191bf6d2fbf3e19ddee40fc3bbb758ea5c9228
Rohanc28 <rochaturvedi01@gmail.com>
2022-08-21 19:55:42 +0530

    few changes

--- myScript.js
+++ myScript.js
@@ -4,7 +4,7 @@ var typed = new Typed(".auto-type", {
       loop: false
     })
     var typed = new Typed(".achi", {
-      strings: [" ^12000 IEEE Spin-o-web May 2022: Frontend Contest: 2nd Place</b> ^1000 <br>^1000<br><b>Murious Hackathon Mar 2022: 2nd Place:</b><br> Created working prototype, which digitalized traditional vending machines. Worked on creating the website + helped the team setup payments using RazorPay API gateway and output using Arduino ^1000 <br>^1000<br>ESummit'21 AlterEgo Oct 2021: UI: 2nd Place: Rethinking and Redesigning Tech companies Logos.  Second Place for Redesigning Yahoo! logo^1000 <br>^1000<br>HackerRank:^400 <br>6 Star: Problem Solving^400<br>5 Star: C++ and Python^400<br>5 Star: 30 days of Code</ul>"],
+      strings: [" <b>^12000 IEEE Spin-o-web May 2022: Frontend Contest: 2nd Place</b> ^1000 <br>^1000<br><b>Murious Hackathon Mar 2022: 2nd Place:</b><br> Created working prototype, which digitalized traditional vending machines. Worked on creating the website + helped the team setup payments using RazorPay API gateway and output using Arduino ^1000 <br>^1000<br><b>ESummit'21 AlterEgo Oct 2021: UI: 2nd Place:</b>Rethinking and Redesigning Tech companies Logos.  Second Place for Redesigning Yahoo! logo^1000 <br>^1000<br><b>HackerRank:</b>^400 <br>6 Star: Problem Solving^400<br>5 Star: C++ and Python^400<br>5 Star: 30 days of Code</ul>"],
       typeSpeed: 60,
       loop: false
     })
